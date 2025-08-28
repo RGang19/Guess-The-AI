2d6c0115abb26c222fe30f1bc54d92b9f487ffb8
Rahul Gangwar <rahulgangwar4567@gmail.com>
2025-08-28 20:28:41 +0530

    added

--- src/screens/game/GamePage.jsx
+++ src/screens/game/GamePage.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import { useAccount } from 'wagmi';
-import './GamePage.css';
+import BackgroundImage from '../../assets/Background.png';
 
 const GamePage = () => {
-  const { isConnected } = useAccount();
+  const containerStyle = {
+    backgroundImage: `url(${BackgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: '100% 0',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
+    margin: 0,
+    padding: 0,
+    overflow: 'hidden',
+    zIndex: -1
+  };
 
-  if (!isConnected) {
-    return (
-      <div className="game-container">
-        <h2>Please connect your wallet to play the game</h2>
-      </div>
-    );
-  }
-
-  return (
-    <div className="game-container">
-      <h2>Game Page</h2>
-      {/* Add your game content here */}
-      <div className="game-content">
-        <p>Game content will be displayed here</p>
-      </div>
-    </div>
-  );
+  return <div style={containerStyle} />;
 };
 
 export default GamePage;
